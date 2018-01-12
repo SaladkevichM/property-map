23a900586b72cbae8ab84b8e966dfac8e30e9b2a
Mikita Saladkevich <Mikita_Saladkevich@epam.com>
2018-01-12 12:29:44 +0300

    Up to date

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # property-map
+
+## Two static methods.
+
+### Read all properties key-value which are located under System.getProperty("user.dir") and sub-folders & put them in one ReferenceMap<>
+
+#### static org.pm.Props.property(String name)
+#### static org.pm.Props.property(String name, String filename) // if you want to read from specific file
---
+++ src/main/java/org/pm/Props.java
@@ -0,0 +1,54 @@
+package org.pm;
+
+import org.apache.commons.collections4.map.ReferenceMap;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
+
+public final class Props {
+
+    private static Map<Object, Object> cache = new ReferenceMap<>();
+
+    public static final String property(String name) {
+        if (cache.get(name) == null) {
+            refresh("");
+        }
+        return (String) cache.get(name);
+    }
+
+    public static final String property(String name, String filename) {
+        cache.clear();
+        refresh(filename);
+        return (String) cache.get(name);
+    }
+
+    private static void refresh(String filename) {
+        Collection<File> files = FileUtils.listFiles(new File(System.getProperty("user.dir")),
+                new String[] {"properties"}, true);
+        for (File file : files) {
+            if (!filename.isEmpty() && !filename.equals(file.getName())) {
+                continue;
+            }
+            if (file.exists()) {
+                Properties props = new Properties();
+                try {
+                    props.load(new FileInputStream(file));
+                } catch (IOException e) {
+                    continue; // skip current iteration
+                }
+                for (Map.Entry<Object, Object> entry : props.entrySet()) {
+                    cache.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+    }
+
+    private Props() {
+
+    }
+}
